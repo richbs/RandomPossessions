0ea8aff0049be13acad234ea4f274811f8c8ef3a
Richard Barrett-Small <rickbs@gmail.com>
2012-03-04 19:29:41 +0000

    Added functionality to main

--- RandomPossessions/main.m
+++ RandomPossessions/main.m
@@ -11,12 +11,29 @@
 int main (int argc, const char * argv[])
 {
 
-    @autoreleasepool {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    NSMutableArray *items = [[NSMutableArray alloc] init];
+    
+    
+    [items addObject:@"foo"];
+    [items addObject:@"bar"];
+    [items addObject:@"baz"];
+    
+    [items insertObject:@"snafu" atIndex:0];
+    
+    for (int i = 0; i < [items count]; i++) {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        int strLen = (int)[[items objectAtIndex:i] length];
+        NSLog(@"%@: %d", [items objectAtIndex:i], strLen);
     }
+    
+    
+    // housekeeping
+    [items release];
+    items = nil;
+    [pool drain];
+    
     return 0;
 }
 
