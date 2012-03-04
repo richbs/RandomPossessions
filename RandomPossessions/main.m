9512e13adcc812c1c2dd872de4ebbdf4b5df68ca
Richard Barrett-Small <rickbs@gmail.com>
2012-03-04 17:15:58 +0000

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Richard Barrett-Small on 04/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
