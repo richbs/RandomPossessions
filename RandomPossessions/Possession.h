7847ba6a0732e1d83713148bff0f75c50af7f01e
Richard Barrett-Small <rickbs@gmail.com>
2012-03-04 19:33:01 +0000

    Starting Possession class

---
+++ RandomPossessions/Possession.h
@@ -0,0 +1,13 @@
+//
+//  Possession.h
+//  RandomPossessions
+//
+//  Created by Richard Barrett-Small on 04/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Possession : NSObject
+
+@end
---
+++ RandomPossessions/Possession.m
@@ -0,0 +1,13 @@
+//
+//  Possession.m
+//  RandomPossessions
+//
+//  Created by Richard Barrett-Small on 04/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Possession.h"
+
+@implementation Possession
+
+@end
